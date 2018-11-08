c8688b736c887cb12c1b7e01e6a23a5075bd2dc1
Tyler Stern <tylerstern0@gmail.com>
2018-11-08 15:16:06 -0600

    shuffle and deal rewritten as array

--- deck.cpp
+++ deck.cpp
@@ -34,16 +34,50 @@ Deck::Deck(){
 };
 
 void Deck::shuffle() {
+    unsigned int currentTime =  (unsigned)time(0);  //get time
+    srand(currentTime);                             //seed w/ time
+    Card tempCard;
+    int swapIndex = 0;
+    int tempIndex = myIndex;
 
-// random generator function:
-    std::random_shuffle (Cards.begin(), Cards.end());
-
-    // print out content:
-    std::cout << "deck contains:";
-    for (std::vector<int>::iterator it=Cards.begin(); it!=Cards.end(); ++it){
-        std::cout << ' ' << *it;
-        std::cout << '\n';
+    while(tempIndex >= 0) {
+        swapIndex = (rand() % tempIndex);
+        tempCard = myCards[swapIndex];
+        myCards[swapIndex] = myCards[tempIndex];
+        myCards[tempIndex] = tempCard;
+        tempIndex--;
     }
+
+/*
+
+    int numSongs = 0;
+
+    while(ctrptr != nullptr){             //count the number of nodes in the list -- should we implement as a function?
+        numSongs++;
+        ctrptr = ctrptr->next;
+    }
+    cout << "numSongs=" << numSongs << endl;
+
+    while(ptr1 != nullptr){
+        ptr2 = ptr1;
+        ptr3 = ptr1;
+        int random = (rand() % numSongs);   //select a random index to swap to
+        cout << random << endl;
+        for(int i = 0; i < random; i++){
+            if(ptr2->next != nullptr) {     //traverse through the list random number of times, rolling over if you hit null
+                ptr2 = ptr2->next;
+            }else{
+                ptr2=head;
+            }
+
+        }
+        cout << "Swapping songs:" << ptr3->s.getName() << "and" << ptr2->s.getName() << endl;
+        swap(ptr3, ptr2);                  //swap current position to current position + random
+        ptr1 = ptr1 -> next;               //move ptr1 to next 'unshuffled' element (it may have already been shuffled and placed there)
+
+    }
+
+*/
 
 }
 
